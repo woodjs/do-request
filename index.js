d106bdbbb324946e7f43c96819b2dab74cb69d6d
bugong <1761869346@qq.com>
2016-05-05 20:52:31 +0800

    修改do-request

--- index.js
+++ index.js
@@ -3,24 +3,17 @@
 let utils = require('./lib/utils');
 let request = require('./lib/request');
 
-let headers = {};
-
 
 function doRequest(opts, callback) {
 
   if (!opts.server && !opts.url) return;
 
-  let options = utils.createRequestOptions(opts, headers);
+  let options = utils.createRequestOptions(opts);
   let url = utils.createRequestUrl(options);
 
   utils.isContainFiles(options) ?
     request.doMultipartRequest(url, options, callback) : request.doNormalRequest(url, options, callback);
 }
-
-
-doRequest.configHeaders = function (customerHeaders) {
-  headers = customerHeaders;
-};
 
 
 module.exports = doRequest;
